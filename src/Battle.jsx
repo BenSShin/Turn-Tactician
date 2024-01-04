74daac3f8e63423fc2b9de89f1b36ea3f4a7783e
bshin27 <125755440+bshin27@users.noreply.github.com>
2024-01-04 15:28:36 -0600

    began styling character blocks

--- src/Battle.jsx
+++ src/Battle.jsx
@@ -1,7 +1,15 @@
 export function Battle() {
   return (
-    <>
-      <div></div>
-    </>
+    <div id="battle-wrapper">
+      <div className="block">
+        <div className="input-wrapper">
+          <input className="name-input" type="text" placeholder="name" />
+        </div>
+        <div className="input-wrapper">
+          <input className="initiative-input" type="text" placeholder="initiative roll" />
+        </div>
+      </div>
+      <button className="add-block-button"></button>
+    </div>
   );
 }
