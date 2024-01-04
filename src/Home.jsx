60dd298ed479f40725eb8f9420b004817c9d65f0
bshin27 <125755440+bshin27@users.noreply.github.com>
2024-01-04 13:49:43 -0600

    added home page

---
+++ src/Home.jsx
@@ -0,0 +1,8 @@
+export function Home() {
+  return (
+    <div>
+      <h1>Welcome to Turn Tactician</h1>
+      <p>This is an application to help keep track of Initiatives and Health</p>
+    </div>
+  );
+}
